bf0f0d16f673773eebf185ec68c60081b0f7b5aa
여범휘 <bhyeo@withbaily.com>
2025-03-27 10:44:43 +0900

    test

--- lib/app.dart
+++ lib/app.dart
@@ -3,24 +3,35 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:map/app_theme.dart';
 import 'package:map/pages/home_pages.dart';
 import 'package:map/pages/router.dart';
+import 'package:widgetbook/widgetbook.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 800),
-      minTextAdapt: true,
-      splitScreenMode: false,
-      builder:(context,child)=> MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme(context).theme,
-        routerConfig: router,
-        title: 'map',
-      ),
+    return Widgetbook(
+      directories: [
+        WidgetbookCategory(
+          name: 'test',
+          children: [
+            WidgetbookUseCase(
+              name: 'doq',
+              builder: (context) => ScreenUtilInit(
+                designSize: const Size(360, 800),
+                minTextAdapt: true,
+                splitScreenMode: false,
+                builder: (context, child) => MaterialApp.router(
+                  debugShowCheckedModeBanner: false,
+                  theme: AppTheme(context).theme,
+                  routerConfig: router,
+                  title: 'map',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
-
-
